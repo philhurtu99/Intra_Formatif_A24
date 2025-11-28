3169fb3afbf7a6743e73d4c954eb314693246110
Philippe Clement <1633730@cegepmontpetit.ca>
2025-11-27 22:53:11 -0500

    B

--- question2/ngSignalR/src/app/app.component.ts
+++ question2/ngSignalR/src/app/app.component.ts
@@ -31,22 +31,54 @@ export class AppComponent {
     this.hubConnection = new signalR.HubConnectionBuilder()
       .withUrl('http://localhost:5282/hubs/pizza')
       .build();
+//******************************************** */
+    this.hubConnection!.on('NbrUsers', (data) => {
+      console.log(data);
+      this.nbUsers = data;
 
+    });
+
+    this.hubConnection!.on('PrixPizza', (data) => {
+      this.pizzaPrice = data;
+      console.log(data);
+
+    });
+
+    this.hubConnection!.on('UpdateMoney', (data) => {
+      this.money = data;
+      console.log(data);
+
+    });
+
+    this.hubConnection?.on("UpdateNbPizzasAndMoney", (a:number, b:number) => {
+      this.money = a;
+      this.nbPizzas = b;
+      console.log(a,b);
+    });
+
+    
+
+    //*************************************** */
     // TODO: Mettre isConnected à true seulement une fois que la connection au Hub est faite
-    this.isConnected = true;
+    this.hubConnection.start().then(() => {console.log('La connexion est active!'); this.isConnected = true}).catch();
+
+    //this.isConnected = true;
   }
 
   selectChoice(selectedChoice:number) {
     this.selectedChoice = selectedChoice;
+    this.hubConnection?.invoke("SelectChoice", this.selectedChoice);
   }
 
   unselectChoice() {
     this.selectedChoice = -1;
   }
 
-  addMoney() {
+  addMoney() {   
+    this.hubConnection?.invoke("AddMoney", this.selectedChoice)
   }
 
   buyPizza() {
+    this.hubConnection?.invoke("BuyPizza", this.selectedChoice)
   }
 }
